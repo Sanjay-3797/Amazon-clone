1cc08d545745609d018f112de3967f56e208b088
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-10-04 16:43:34 +0530

    header-finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,57 +4,8 @@ import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { useState } from "react";
-
-const showItemList = [
-  {
-    _id: "100",
-    title: "All Departments",
-  },
-  {
-    _id: "101",
-    title: "All Departments",
-  },
-  {
-    _id: "102",
-    title: "All Departments",
-  },
-  {
-    _id: "103",
-    title: "All Departments",
-  },
-  {
-    _id: "104",
-    title: "All Departments",
-  },
-  {
-    _id: "105",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-  {
-    _id: "106",
-    title: "All Departments",
-  },
-];
+import { showItemList } from "../../constants/constant";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Header = () => {
   const [showAll, setShowAll] = useState(false);
@@ -69,25 +20,29 @@ const Header = () => {
           <LocationOnOutlinedIcon />
           <p className="text-sm text-lightText font-light flex flex-col">
             Deliver to{" "}
-            <span className="text-sm font-semibold -mt-1 text-whiteText ">
+            <span className="text-sm font-semibold -mt-1 text-whiteText">
               Bengaluru
             </span>
           </p>
         </div>
+
         <div className="h-10 rounded-md flex flex-grow relative">
           <span
             onClick={() => {
               setShowAll((prev) => !prev);
             }}
             className="w-14 h-full bg-gray-200 hover:bg-gray-300 border-2 cursor-pointer duration-300 text-sm text-amazon_blue font-titleFont flex items-center justify-center rounded-tl-md rounded-bl-md">
-            All <span></span>
-            <ArrowDropDownOutlinedIcon />
+            All <ArrowDropDownOutlinedIcon />
           </span>
           {showAll && (
             <div>
               <ul className="absolute w-56 h-80 top-10 left-0 overflow-y-scroll overflow-x-hidden bg-white border-[1px] border-amazon_blue text-black p2 flex flex-col gap-1 z-50">
                 {showItemList.map((item) => (
-                  <li key={item._id}>{item.title}</li>
+                  <li
+                    className="p-1 text-sm tracking-wide font-titleFont border-b-[1px] border-b-transparent hover:border-b-amazon_blue cursor-pointer duration-200"
+                    key={item._id}>
+                    {item.title}
+                  </li>
                 ))}
               </ul>
             </div>
@@ -99,6 +54,32 @@ const Header = () => {
           <span className="w-12 h-full flex items-center justify-center bg-amazon_yellow hover:bg-[#f3a847] duration-300 text-amazon_blue cursor-pointer rounded-tr-md rounded-br-md">
             <SearchOutlinedIcon />
           </span>
+        </div>
+        <div className="headerHover flex flex-col items-start justify-center">
+          <p className="text-sm text-lightText font-light">Hello, sign in</p>
+          <p className="text-sm font-semibold -mt-1 text-whiteText">
+            Accounts & Lists{" "}
+            <span>
+              <ArrowDropDownOutlinedIcon />
+            </span>
+          </p>
+        </div>
+
+        <div className="headerHover flex flex-col items-start justify-center relative">
+          <p className="text-sm text-lightText font-light">Returns</p>
+          <p className="text-sm font-semibold -mt-1 text-whiteText">
+            & Orders{" "}
+            <span>
+              <ArrowDropDownOutlinedIcon />
+            </span>
+          </p>
+        </div>
+
+        <div className="headerHover flex flex-col items-start justify-center relative">
+          <ShoppingCartOutlinedIcon />
+          <p className="text-sm font-semibold -mt-1 text-whiteText">
+            Cart <span className="absolute text-xs -top-1 left-6 font-semibold p-1 h-4 bg-[#f3a847] text-amazon_blue rounded-full flex justify-center items-center">0</span>
+          </p>
         </div>
       </div>
     </div>
